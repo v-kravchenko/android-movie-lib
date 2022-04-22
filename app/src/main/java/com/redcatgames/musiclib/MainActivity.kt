f59b814ec53e149d704036c1de407eb4a541f850
Vladimir Kravchenko <vladimir.kravchenko@sytecs.com.ua>
2022-04-22 11:56:00 +0300

    Add viewBinding feature

--- app/src/main/java/com/redcatgames/musiclib/MainActivity.kt
+++ app/src/main/java/com/redcatgames/musiclib/MainActivity.kt
@@ -2,10 +2,18 @@ package com.redcatgames.musiclib
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.redcatgames.musiclib.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.text1.text = BuildConfig.APPLICATION_ID
     }
 }

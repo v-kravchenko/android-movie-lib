55f8c66fdd42d838ec736e0bb0f37859b5e3f345
Vladimir Kravchenko <vladimir.kravchenko@sytecs.com.ua>
2022-05-04 19:21:00 +0300

    Cleanup MovieViewModel.kt

--- app/src/main/java/com/redcatgames/movies/presentation/movie/MovieViewModel.kt
+++ app/src/main/java/com/redcatgames/movies/presentation/movie/MovieViewModel.kt
@@ -3,14 +3,15 @@ package com.redcatgames.movies.presentation.movie
 import android.content.Context
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
-import com.redcatgames.movies.domain.usecase.movie.*
+import com.redcatgames.movies.domain.usecase.movie.GetMovieInfoUseCase
+import com.redcatgames.movies.domain.usecase.movie.GetMovieUseCase
+import com.redcatgames.movies.domain.usecase.movie.LoadMovieUseCase
 import com.redcatgames.movies.domain.util.UseCaseResult
 import com.redcatgames.movies.presentation.base.BaseViewModel
 import com.redcatgames.movies.presentation.util.SingleLiveEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel

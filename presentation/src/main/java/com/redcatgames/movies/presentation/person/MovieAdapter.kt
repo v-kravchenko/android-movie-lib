093c7adfe461e95530b03cdad2a12c25a3327631
Vladimir Kravchenko <vladimir.kravchenko@sytecs.com.ua>
2022-08-08 15:39:22 +0300

    Add layout_person_movie_horizontal.xml

--- presentation/src/main/java/com/redcatgames/movies/presentation/person/MovieAdapter.kt
+++ presentation/src/main/java/com/redcatgames/movies/presentation/person/MovieAdapter.kt
@@ -8,7 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.redcatgames.movies.domain.model.PersonCast
 import com.redcatgames.movies.presentation.PosterSize
 import com.redcatgames.movies.presentation.R
-import com.redcatgames.movies.presentation.databinding.LayoutMovieHorizontalBinding
+import com.redcatgames.movies.presentation.databinding.LayoutPersonMovieHorizontalBinding
 import com.redcatgames.movies.presentation.getPosterUri
 import com.redcatgmes.movies.baseui.util.loadByUrl
 
@@ -21,8 +21,10 @@ class MovieAdapter : ListAdapter<PersonCast, Holder>(ItemDiffCallback()) {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val binding: LayoutMovieHorizontalBinding =
-            LayoutMovieHorizontalBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding: LayoutPersonMovieHorizontalBinding =
+            LayoutPersonMovieHorizontalBinding.inflate(LayoutInflater.from(parent.context),
+                parent,
+                false)
         return Holder(binding, onItemClick)
     }
 
@@ -30,13 +32,14 @@ class MovieAdapter : ListAdapter<PersonCast, Holder>(ItemDiffCallback()) {
 }
 
 class Holder(
-    private val itemBinding: LayoutMovieHorizontalBinding,
+    private val itemBinding: LayoutPersonMovieHorizontalBinding,
     private val eventClickItem: ((PersonCast) -> Unit)?,
 ) : RecyclerView.ViewHolder(itemBinding.root) {
 
     fun bind(item: PersonCast) {
         this.itemView.setOnClickListener { eventClickItem?.invoke(item) }
         itemBinding.textTitle.text = item.title
+        itemBinding.textSubtitle.text = item.character
         itemBinding.textRating.text = item.voteRating
         itemBinding.posterImage.loadByUrl(item.getPosterUri(PosterSize.MEDIUM)) {
             placeholder(R.drawable.poster_placeholder_medium)
